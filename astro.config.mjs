a2dccc4f44eec1090c53ff5348001b19f93abea3
Jeffry Gonzalez <jeff@hypertheory.com>
2025-04-04 11:32:49 -0400

    Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,28 +2,32 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Web API with .NET',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Courses',
-          autogenerate: { directory: 'courses' }
-				},
-				{
-					autogenerate: { directory: 'how-to' },
-          label: 'Guides',
-				},
-        {
-          label: 'Explainers',
-          autogenerate: { directory: 'explainers'}
-        }
-			],
-		}),
+  integrations: [
+      starlight({
+          title: 'Web API with .NET',
+          social: {
+              github: 'https://github.com/withastro/starlight',
+          },
+          sidebar: [
+              {
+                  label: 'Courses',
+        autogenerate: { directory: 'courses' }
+              },
+              {
+                  autogenerate: { directory: 'how-to' },
+        label: 'Guides',
+              },
+      {
+        label: 'Explainers',
+        autogenerate: { directory: 'explainers'}
+      }
+          ],
+      }),
 	],
+
+  adapter: vercel(),
 });
